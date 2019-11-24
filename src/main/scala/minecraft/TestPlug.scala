1d08926b98bfd21d4b837fc746835c83e4107ff8
fearlesspandas <landonrenzullo@gmail.com>
2019-11-23 20:38:58 -0500

    scala integrated

--- src/main/scala/minecraft/TestPlug.scala
+++ src/main/scala/minecraft/TestPlug.scala
@@ -7,15 +7,15 @@ import org.bukkit.command.Command;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Player;
-
+import org.bukkit.inventory.ItemStack;
 //
 // import org.bukkit.inventory.Packet250CustomPayload;
 // import org.bukkit.inventory.Packet100OpenWindow;
 // import org.bukkit.inventory.MerchantRecipeList;
 // import org.bukkit.inventory.MerchantRecipe;
- import org.bukkit.inventory.ItemStack;
 
 
+import minecraft.economy._
 //import org.apache.spark.sql._
 
 class Minecap extends JavaPlugin{
@@ -28,10 +28,21 @@ class Minecap extends JavaPlugin{
 		getLogger().info("onDisable has been invoked!");
 	}
 
-
 	override def onCommand(sender : CommandSender, cmd : Command, label : String, args : Array[String]):Boolean = {
     //val spark = SparkSesitem.toString + messagesion.builder().getOrCreate()
     //import spark.implicits._
+    implicit val orderbookloc = "testplug/orderbook.json"
+    def playerCommand(sender : CommandSender, cmd : Command, label : String, args : Array[String])(implicit player:Player):Boolean ={
+      val response = cmd.getName() match {
+        case "$" => {
+          val price = new ItemStack(Material.getMaterial(args(0)),args(1).toInt)
+          val item = new ItemStack(Material.getMaterial(args(2)),args(3).toInt)
+          OrderWriter.writeOrder(OrderWriter.nextid,player,price,item,args(3).toInt).toString
+        }
+        case _ => "Unrecognized command"
+      }
+      return true
+    }
 	 var player : Player =
 	   sender match{
 	    case p : Player => p.asInstanceOf[Player]
@@ -40,11 +51,9 @@ class Minecap extends JavaPlugin{
 	    }
 	  }
 
+
     //player.sendMessage("isplayer")
-    val response = cmd.getCommandName() match {
-      case "$" => "place order"//placeOrder(price:ItemStack, item:ItemStack)
-      case _ => "Unrecognized command"
-    }
+
     //val  inv = player.getInventory()
     //val item = inv.getItemInMainHand()
     //val test = Seq(0 to 10:_*)
@@ -53,7 +62,7 @@ class Minecap extends JavaPlugin{
     // test.forEach( (i) => {
     //   message += i
     // })
-    player.sendMessage(response)
+    //player.sendMessage(response)
 
 
    return true;
--- src/main/scala/minecraft/economy/OrderBook.scala
+++ src/main/scala/minecraft/economy/OrderBook.scala
@@ -12,6 +12,8 @@ import org.bukkit.inventory.ItemStack;
 import com.google.gson.Gson
 
 import java.io._
+
+import scala.reflect.runtime.universe._
 //
 // import org.bukkit.inventory.Packet250CustomPayload;
 // import org.bukkit.inventory.Packet100OpenWindow;
@@ -20,11 +22,24 @@ import java.io._
 
 
 object OrderWriter {
+  var nextId = 0
+  def nextid = {
+    nextId +=1
+    nextId-1
+  }
   def writeOrder(order: Order)(implicit loc:String){
     val pw = new PrintWriter(new File(loc))
     pw.write(order.toJson)
     pw.close
   }
+  def writeOrder(orderid:Int,player: Player, price: ItemStack,item: ItemStack,remaining: Int)(implicit loc :String){
+    writeOrder(Order(orderid,player,price,item,remaining))
+  }
+  def readOrderBook(loc:String): OrderBook = {
+    val fr = new FileReader(new File(loc))
+    (new Gson).fromJson(fr,classOf[OrderBook])
+  }
+
 
 }
 case class Order(orderid:Int,player:Player, price:ItemStack,item: ItemStack,remaining: Int) {
@@ -32,7 +47,5 @@ case class Order(orderid:Int,player:Player, price:ItemStack,item: ItemStack,rema
 }
 
 
-case class OrderBook(loc:String){
-  import java.io._
-
+case class OrderBook(orders:Order*){
 }
